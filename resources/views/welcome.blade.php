892ba8d5a99b1fdfebe825637d07e9f4a77cd89d
Patrick Kernke <patrick@kernke.net>
2015-09-27 22:18:43 +0200

    Integration of blacktie inset theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,141 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Eifelshop – Kletterladen in Koblenz.">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <title>Eifelshop - Kletterladen in Koblenz</title>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <link href="/css/app.css" rel="stylesheet">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+  </head>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+  <body>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+    <div id="headerwrap">
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="row">
+                <div class="col-lg-6 col-lg-offset-3">
+                    <div class="splash">
+                        <h1><img src="/images/eifelshop-logo_splash.png" alt="Eifelshop Logo"></h1>
+                        <h4>
+                            Dein Kletterladen in Koblenz eröffnet am 10.10.2015 <br>
+                            <a href="#informationen">
+                                <i class="fa fa-angle-double-down"></i>
+                                Mehr 
+                                <i class="fa fa-angle-double-down"></i>
+                            </a> 
+                        </h4>
+                    </div>
+                </div>
+            </div><!--/row -->
+        </div> <!-- /container -->
+
+        <div class="filter"></div>
+    </div><!--/headerwrap -->
+
+    {{-- <section id="event">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 centered">
+                    <h1>Eröffnungsfeier</h1>
+                    <p>Details...</p>
+                </div>
             </div>
         </div>
-    </body>
+    </section>
+
+    <section id="aboutus">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 centered">
+                    <h1>Der Eifelshop</h1>
+                    <p>Wer ist der Eifelshop? Was macht er?</p>
+                </div>
+            </div>
+        </div>
+    </section> --}}
+    
+    <div class="container">
+        <div class="row centered mt mb">
+            <h1>Eifelshop-Marken</h1>
+            
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio01.png" class="img-responsive"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio02.png" class="img-responsive"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio03.png" class="img-responsive"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio04.png" class="img-responsive"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio05.png" class="img-responsive"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+                <a href="work.html"><img src="/images/portfolio/folio06.png" class="img-responsive"></a>
+            </div>
+        </div><!--/row -->
+    </div><!--/container -->
+    
+    <div id="social">
+        <div class="container">
+            <div class="row centered">
+                
+                <div class="col-lg-6">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </div>
+                <div class="col-lg-6">
+                    <a href="http://www.eifelblock.de">Eifelblock</a>
+                </div>
+
+            </div><!--/row -->
+        </div><!--/container -->
+    </div><!--/social -->
+
+    <div id="footerwrap">
+        <div class="container">
+            <div class="row centered">
+                <div class="col-lg-4">
+                    <p>
+                        <b>Adresse</b> <br>
+                        Eifelshop (Inh. Simon Fluck) <br>
+                        Ernst-Abbe-Str. 13 <br>
+                        56079 Koblenz
+                    </p>
+                </div>
+            
+                <div class="col-lg-4">
+                    <p>
+                        <b>Kontaktdaten</b> <br>
+                        Telefon 0261 - 988 111 222 <br>
+                        Mail info@eifelshop.de
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <p>
+                        <b>Rechtliches</b> <br>
+                        BIC DE88129399102938 <br>
+                        IBAN MALEDE23KO <br>
+                        Steuernummer 32/2123/2223
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div><!--/footerwrap -->
+    
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
 </html>
